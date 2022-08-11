ce5b790713825b5abfe8ac2df56c6a98e4872e7c
Andreas Fink <afi@innovaphone.com>
2022-08-11 09:34:28 +0200

    added comments

--- jslicenseexample/jslicenseexample/innovaphone-jslicenseexampleservice.js
+++ jslicenseexample/jslicenseexample/innovaphone-jslicenseexampleservice.js
@@ -16,7 +16,7 @@ new PbxApi("PbxAdminApi").onconnected(function (conn_pbx) {
     var info = JSON.parse(conn_pbx.info);
     if ("lics" in info) servicelics = info.lics;
 
-    conn_pbx.onmessage(function (msg) {
+    conn_pbx.onmessage(function (msg) { // handle responses from the PBX
         var obj = JSON.parse(msg);
 
         if (obj.src in requests) {
@@ -25,7 +25,7 @@ new PbxApi("PbxAdminApi").onconnected(function (conn_pbx) {
         }
     });
 
-    conn_pbx.onclose(function () {
+    conn_pbx.onclose(function () { // cleanup if PBX connection is closed
         pbxConnections.splice(pbxConnections.indexOf(conn_pbx), 1);
         conn_pbx = null;
         requests = new Object();
@@ -36,39 +36,59 @@ new PbxApi("PbxAdminApi").onconnected(function (conn_pbx) {
 
 
 new JsonApi("user").onconnected(function (conn) {
-
     if (conn.app !== "innovaphone-jslicenseexample") return;
     conn.onmessage(function (msg) {
         var src;
         var obj = JSON.parse(msg);
 
-        if (pbxConnections.length < 1) {
-            conn.send(JSON.stringify({ api: "user", mt: obj.mt + "Result", error: true, message: "No WebSocket connection from PBX", src: obj.src }));
-            return;
-        }
-
+        // The JsonApiConnection object represents a WebSocket connection from
+        // the myApps cleint to the App Service. The property "unlicensed" is
+        // set to true, in case the logged in PBX User has no App license assigned.
+        //
+        // https://sdk.innovaphone.com/13r3/doc/javascript/JsonApi.htm#JsonApiConnection
+        // 
         if (obj.mt === "CheckLicenseOnClientConnection") {
             if (conn.unlicensed) {
-                conn.send(JSON.stringify({ api: "user", mt: "CheckLicenseOnClientConnectionResult", licensed: false, message: "No App License at User Object " + conn.sip, src: obj.src }));
+                conn.send(JSON.stringify({ api: "user", mt: "CheckLicenseOnClientConnectionResult", licensed: false, message: "No App License at connection from User " + conn.sip, src: obj.src }));
             } else {
                 conn.send(JSON.stringify({ api: "user", mt: "CheckLicenseOnClientConnectionResult", licensed: true, src: obj.src }));
             }
         }
 
-        if (obj.mt === "CheckLicenseOnPBX") {
-            var licstring = "App(" + conn.app + ")";
+        if (pbxConnections.length < 1) { // check if PBX connection is not available before handle requests
+            conn.send(JSON.stringify({ api: "user", mt: obj.mt + "Result", error: true, message: "No WebSocket connection from PBX", src: obj.src }));
+            return;
+        }
 
+        // To check if some specific App license as assigned to some specific
+        // PBX user the message CheckAppLic of the PbxAdminApi can be used.
+        //
+        // https://sdk.innovaphone.com/13r3/doc/appwebsocket/PbxAdminApi.htm#CheckAppLic
+        //
+        if (obj.mt === "CheckLicenseOnPBX") {
+            var licstring = "App(" + conn.app + ")"; // define the license string
+
+            // define callback function for response handling first
             src = "src" + srccount++;
             requests[src] = function (obj) {
                 if (obj.mt === "CheckAppLicResult") {
-                    var licensed = obj.ok ? true : false;
+                    var licensed = obj.ok ? true : false; // the property "ok" is set to true if license is assigned to the user
                     conn.send(JSON.stringify({ api: "user", mt: "CheckLicenseOnPBXResult", licensed: licensed, servicelics: servicelics, src: obj.src }));
                 }
             };
+            // send request to the PBX
             pbxConnections[0].send(JSON.stringify({ api: "PbxAdminApi", mt: "CheckAppLic", h323: conn.sip, lic: licstring, src: src }));
         }
 
+        // The number of installed Port Licenses is a good point to determinate
+        // overall system size, in case it should affect required number of licenses.
+        // 
+        // The GetPbxLicenses message of the PbxAdminApi is used to list all installed licenses
+        //
+        // https://sdk.innovaphone.com/13r3/doc/appwebsocket/PbxAdminApi.htm#GetPbxLicenses
+        //
         if (obj.mt === "CheckSystemSize") {
+            // define callback function for response handling first
             src = "src" + srccount++;
             requests[src] = function (obj) {
                 if (obj.mt === "GetPbxLicensesResult") {
@@ -76,10 +96,13 @@ new JsonApi("user").onconnected(function (conn) {
                         var ports = obj.lic.filter(function (e) { return e.name === "Port" })[0].count;
                         conn.send(JSON.stringify({ api: "user", mt: "CheckSystemSizeResult", ports: ports, src: obj.src }));
                     } else {
+                        // in case "lic" property is missing in the GetPbxLicensesResult
+                        // the PBX is in the Test Mode or has no licenses installed
                         conn.send(JSON.stringify({ api: "user", mt: "CheckSystemSizeResult", testmode: true, src: obj.src }));
                     }
                 }
             };
+            // send request to the PBX
             pbxConnections[0].send(JSON.stringify({ api: "PbxAdminApi", mt: "GetPbxLicenses", src: src }));
         }
     });
